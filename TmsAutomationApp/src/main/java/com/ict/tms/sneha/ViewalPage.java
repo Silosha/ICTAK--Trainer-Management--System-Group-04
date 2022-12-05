f2f84c57495bdd18f325f2958d0c6e3827e67dcb
sneharejil <snehasrejil123@gmail.com>
2022-12-05 22:20:29 +0530

    commit

---
+++ TmsAutomationApp/src/main/java/com/ict/tms/sneha/ViewalPage.java
@@ -0,0 +1,35 @@
+package com.ict.tms.sneha;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ViewalPage {
+
+	WebDriverWait wait;
+	@FindBy(css ="input[type='text']") WebElement search;
+	@FindBy(id="ViewAllocation") WebElement button;
+
+	
+	public ViewalPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this);
+		this.wait=new WebDriverWait(driver, 30);
+
+	}
+	public  void trainerdetails2(String searchname)
+	{try {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("ViewAllocation"))).click();
+
+		wait.until(ExpectedConditions.visibilityOf(search)).sendKeys(searchname);
+		search.clear();
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+	}
+}
